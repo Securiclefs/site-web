289ead850df4be86d36a9af7a247cd0d31d50308
marielaure1 <edjour.marielaure@gmail.com>
2023-05-28 20:12:35 +0200

    nettoyage

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/google59be6c17e45ff582.html', // Remplacez "googleXXXXX.html" par le nom réel de votre fichier de confirmation de Google
+        destination: '/google59be6c17e45ff582.html', // Remplacez "googleXXXXX.html" par le nom réel de votre fichier de confirmation de Google
+      },
+    ];
+  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.csv$/,
